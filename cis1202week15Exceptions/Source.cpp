eedfcfa593c787e29062af1c4d2670d76d39f77b
abenincasa <anthony.benincasa@sinclair.edu>
2024-04-27 17:06:09 -0400

    Add project files.

---
+++ cis1202week15Exceptions/Source.cpp
@@ -0,0 +1,23 @@
+/*
+Anthony (Tony) Benincasa 4/27/2024 CIS1202.N01
+Week 15 Exceptions
+This program 
+*/
+
+#include "perslib.h"
+
+// function prototypes
+char character(char, int);
+
+int main() {
+
+
+	cout << character('a', 1) << endl;
+
+
+}
+char character(char start, int offset) {
+
+	return start + offset;
+
+}
