3269e24af1a19e3032e8548792702b534e222f34
abenincasa <anthony.benincasa@sinclair.edu>
2024-04-27 20:07:17 -0400

    Final Version

--- cis1202week15Exceptions/Source.cpp
+++ cis1202week15Exceptions/Source.cpp
@@ -11,13 +11,39 @@ char character(char, int);
 
 int main() {
 
-
-	cout << character('a', 1) << endl;
+	try
+	{
+		cout << character('a', -1) << endl;
+	}
+	catch (string exceptionString)
+	{
+		cout << exceptionString << endl;
+	}
 
 
 }
 char character(char start, int offset) {
+	
+	if (start > 90 && start < 97)
+	{
+		string exceptionICEString = "ICE-ERROR: Invalid Character Exception";
+		throw exceptionICEString;
+	} else if (start < 65 || start > 122)
+		{
+			string exceptionICEString = "ICE-ERROR: Invalid Character Exception";
+			throw exceptionICEString;
+		}
 
+	if ((start+offset) > 90 && (start+offset) < 97)
+	{
+		string exceptionIREString = "IRE-ERROR: Invalid Range Exception";
+		throw exceptionIREString;
+	} else if ((start+offset) < 65 || (start+offset) > 122)
+		{
+			string exceptionIREString = "IRE-ERROR: Invalid Range Exception";
+			throw exceptionIREString;
+		}
+	
 	return start + offset;
 
 }
